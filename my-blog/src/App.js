9e7b046e257618c88e41f6ac207590774e325068
Mirza-Abdul-Aziz <mirzaabdulaziz006@gmail.com>
2023-01-18 13:09:24 +0500

    Installed react-router-dom package and added the routes to all the pages.

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import ArticlePage from "./pages/ArticlePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Awesome Blog</h1>
-      <div id="page-body">Welcome to my blog!</div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>My Awesome Blog</h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
