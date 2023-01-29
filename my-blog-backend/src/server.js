e9da2c41922005e656d07e680536204ec3e5c223
Mirza-Abdul-Aziz <mirzaabdulaziz006@gmail.com>
2023-01-29 12:14:01 +0500

    Added the add comments functionality using PUT endpoint...

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,14 +4,17 @@ let articlesInfo = [
   {
     name: "learn-react",
     upvotes: 0,
+    comments: [],
   },
   {
     name: "learn-node",
     upvotes: 0,
+    comments: [],
   },
   {
     name: "mongodb",
     upvotes: 0,
+    comments: [],
   },
 ];
 
@@ -25,6 +28,19 @@ app.put("/api/articles/:name/upvote", (req, res) => {
   if (article) {
     article.upvotes += 1;
     res.send(`The ${article.name} article now has ${article.upvotes} upvotes!`);
+  } else {
+    res.send("Article doesn't exists!");
+  }
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+  console.log(postedBy);
+  const article = articlesInfo.find((a) => a.name === name);
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
   } else {
     res.send("Article doesn't exists!");
   }
