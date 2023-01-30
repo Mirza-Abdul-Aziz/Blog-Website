00da1681dab7a8fbb3c8845faf4cc85ded646d00
Mirza-Abdul-Aziz <mirzaabdulaziz006@gmail.com>
2023-01-30 09:05:38 +0500

    Rewritten the upvote endpoint to store the informationin MongoDB database.

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,14 +1,42 @@
 import express from "express";
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(express.json());
 // GET endpoint
-
-app.put("/api/articles/:name/upvote", (req, res) => {
+app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
-  const article = articlesInfo.find((a) => a.name === name);
+
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+
+  const db = client.db("react-blog-db");
+
+  const article = await db.collection("articles").findOne({ name });
+
   if (article) {
-    article.upvotes += 1;
+    res.send(article);
+  } else {
+    res.send("Article not found!");
+  }
+});
+
+//PUT endpoint
+app.put("/api/articles/:name/upvote", async (req, res) => {
+  const { name } = req.params;
+
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+  const db = client.db("react-blog-db");
+
+  await db.collection("articles").updateOne(
+    { name },
+    {
+      $inc: { upvotes: 1 },
+    }
+  );
+  const article = await db.collection("articles").findOne({ name });
+  if (article) {
     res.send(`The ${article.name} article now has ${article.upvotes} upvotes!`);
   } else {
     res.send("Article doesn't exists!");
