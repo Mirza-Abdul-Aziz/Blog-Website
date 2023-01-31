e8bcec228f67e7ca7674af2e9b8ff5e50c69bdae
Mirza-Abdul-Aziz <mirzaabdulaziz006@gmail.com>
2023-01-31 08:48:08 +0500

    Added the react hooks... Also installed the axios library to tie the backend with frontend

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,8 +1,15 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvote: 0, comments: [] });
+
+  useEffect(() => {
+    setArticleInfo({});
+  });
+
   const { articleId } = useParams();
   const article = articles.find((article) => article.name === articleId);
   if (!article) {
