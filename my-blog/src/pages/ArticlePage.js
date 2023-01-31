21afeaa7cfa2379f317ab1f945dd6ecd5a2af572
Mirza-Abdul-Aziz <mirzaabdulaziz006@gmail.com>
2023-01-31 09:37:52 +0500

    Added the functionality to load all the upvotes and comments from teh server.

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,16 +1,23 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import CommentsList from "./components/CommentsList";
 
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({ upvote: 0, comments: [] });
+  const { articleId } = useParams();
 
   useEffect(() => {
-    setArticleInfo({});
-  });
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+    loadArticleInfo();
+  }, []);
 
-  const { articleId } = useParams();
   const article = articles.find((article) => article.name === articleId);
   if (!article) {
     return <NotFoundPage />;
@@ -18,9 +25,11 @@ const ArticlePage = () => {
     return (
       <>
         <h1>{article.title}</h1>
+        <p>This article has {articleInfo.upvotes} upvote(s)!</p>
         {article.content.map((paragraph, i) => (
           <p key={i}>{paragraph}</p>
         ))}
+        <CommentsList comments={articleInfo.comments} />
       </>
     );
   }
