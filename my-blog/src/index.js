f00df2f969fa219b995f6651949872bac06ae2d7
Mirza-Abdul-Aziz <mirzaabdulaziz006@gmail.com>
2023-02-04 11:23:55 +0500

    Added firebase configurations to react...

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzbCixRmdMWgf_QXoB3g8MQmx0zvTJsZQ",
+  authDomain: "my-react-blog-e299c.firebaseapp.com",
+  projectId: "my-react-blog-e299c",
+  storageBucket: "my-react-blog-e299c.appspot.com",
+  messagingSenderId: "878471088967",
+  appId: "1:878471088967:web:7beab189aa8d63cfdada62",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
