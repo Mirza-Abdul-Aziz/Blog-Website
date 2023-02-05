84fd98a3f90af718bd55dcde5b99a435bd6b2a98
Mirza-Abdul-Aziz <mirzaabdulaziz006@gmail.com>
2023-02-05 13:07:49 +0500

    Created the custom hook useUser to show the content depending if the user is logged in or not...

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -3,53 +3,64 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import useUser from "../hooks/useUser";
 import CommentsList from "../components/CommentsList";
 import AddCommentForm from "../components/AddCommentForm";
 
 const ArticlePage = () => {
-  const [articleInfo, setArticleInfo] = useState({ upvote: 0, comments: [] });
-  const { articleId } = useParams();
+    const [articleInfo, setArticleInfo] = useState({ upvote: 0, comments: [] });
+    const { articleId } = useParams();
 
-  useEffect(() => {
-    const loadArticleInfo = async () => {
-      const response = await axios.get(`/api/articles/${articleId}`);
-      const newArticleInfo = response.data;
-      setArticleInfo(newArticleInfo);
+    const { user, isLoading } = useUser();
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        };
+        loadArticleInfo();
+    }, []);
+
+    const article = articles.find((article) => article.name === articleId);
+
+    const addUpvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticleInfo = response.data;
+        setArticleInfo(updatedArticleInfo);
     };
-    loadArticleInfo();
-  }, []);
 
-  const article = articles.find((article) => article.name === articleId);
-
-  const addUpvote = async () => {
-    const response = await axios.put(`/api/articles/${articleId}/upvote`);
-    const updatedArticleInfo = response.data;
-    setArticleInfo(updatedArticleInfo);
-  };
-
-  if (!article) {
-    return <NotFoundPage />;
-  } else {
-    return (
-      <>
-        <h1>{article.title}</h1>
-        <div className="upvotes-section">
-          <button onClick={addUpvote}>Upvote</button>
-          <p>This article has {articleInfo.upvotes} upvote(s)!</p>
-        </div>
-        {article.content.map((paragraph, i) => (
-          <p key={i}>{paragraph}</p>
-        ))}
-        <AddCommentForm
-          articleName={articleId}
-          onUpdatedArticle={(updatedArticleInfo) =>
-            setArticleInfo(updatedArticleInfo)
-          }
-        />
-        <CommentsList comments={articleInfo.comments} />
-      </>
-    );
-  }
+    if (!article) {
+        return <NotFoundPage />;
+    } else {
+        return (
+            <>
+                <h1>{article.title}</h1>
+                <div className="upvotes-section">
+                    {user ? (
+                        <button onClick={addUpvote}>Upvote</button>
+                    ) : (
+                        <button>Login to upvote</button>
+                    )}
+                    <p>This article has {articleInfo.upvotes} upvote(s)!</p>
+                </div>
+                {article.content.map((paragraph, i) => (
+                    <p key={i}>{paragraph}</p>
+                ))}
+                {user ? (
+                    <AddCommentForm
+                        articleName={articleId}
+                        onUpdatedArticle={(updatedArticleInfo) =>
+                            setArticleInfo(updatedArticleInfo)
+                        }
+                    />
+                ) : (
+                    <button>Login to add a comment</button>
+                )}
+                <CommentsList comments={articleInfo.comments} />
+            </>
+        );
+    }
 };
 
 export default ArticlePage;
