2262e3e5d6b8500c8f92326dcbae4f17d74c903e
Mirza-Abdul-Aziz <mirzaabdulaziz006@gmail.com>
2023-02-07 10:42:58 +0500

    Protcting endpoints using the authtoken..

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -10,33 +10,63 @@ admin.initializeApp({
 
 const app = express();
 app.use(express.json());
+app.use(async (res, req, next) => {
+    const { authtoken } = req.headers;
+    if (authtoken) {
+        try {
+            req.user = await admin.auth().verifyIdToken(authtoken);
+        } catch (e) {
+            res.sendStatus(400);
+        }
+    }
+    next();
+});
 
 // GET endpoint
 app.get("/api/articles/:name", async (req, res) => {
     const { name } = req.params;
+    const { uid } = req.user;
 
     const article = await db.collection("articles").findOne({ name });
 
     if (article) {
-        res.send(article);
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvote = uid && !upvoteIds.include(uid);
+        res.json(article);
     } else {
-        res.send("Article not found!");
+        res.sendStatus(404);
     }
 });
 
+app.use((res, req, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+});
 //PUT endpoint
 app.put("/api/articles/:name/upvote", async (req, res) => {
     const { name } = req.params;
 
-    await db.collection("articles").updateOne(
-        { name },
-        {
-            $inc: { upvotes: 1 },
-        }
-    );
     const article = await db.collection("articles").findOne({ name });
+
     if (article) {
-        res.json(article);
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvote = uid && !upvoteIds.include(uid);
+        if (canUpvote) {
+            await db.collection("articles").updateOne(
+                { name },
+                {
+                    $inc: { upvotes: 1 },
+                    $push: { upvoteIds: uid },
+                }
+            );
+        }
+        const updatedArticle = await db
+            .collection("articles")
+            .findOne({ name });
+        res.json(updatedArticle);
     } else {
         res.send("Article doesn't exists!");
     }
@@ -44,12 +74,12 @@ app.put("/api/articles/:name/upvote", async (req, res) => {
 
 app.post("/api/articles/:name/comments", async (req, res) => {
     const { name } = req.params;
-    const { postedBy, text } = req.body;
-
+    const { text } = req.body;
+    const { email } = req.user;
     await db.collection("articles").updateOne(
         { name },
         {
-            $push: { comments: { postedBy, text } },
+            $push: { comments: { postedBy: email, text } },
         }
     );
 
